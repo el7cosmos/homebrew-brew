60d7c6738f7c82123f97d24c416649a3dd19369b
Abi <abi@sepulsa.com>
2018-09-28 09:09:56 +0700

    codeception 2.5.0

--- Formula/codeception.rb
+++ Formula/codeception.rb
@@ -2,8 +2,8 @@ class Codeception < Formula
   desc "Full-stack testing PHP framework"
   homepage "https://codeception.com"
   url "https://codeception.com/codecept.phar"
-  version "2.4.6"
-  sha256 "e41ba47e3576e72b8bd806e11d438a6155198f183cf7efd459e5e575faf8be36"
+  version "2.5.0"
+  sha256 "afcbfe435714b99309e109cba268b9397c22922f4518dc0ba96bf2bc6fe5d50b"
   depends_on "php"
 
   def install
