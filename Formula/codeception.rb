2b8728a5855dad7e642f651fc16055971e1f6885
Abi أب <el7cosmos@gmail.com>
2018-09-28 09:02:34 +0700

    Revert "codeception 2.5.0"
    This reverts commit d43e7bd469cb3e10b88495991fa24fd5822b4392.

--- Formula/codeception.rb
+++ Formula/codeception.rb
@@ -1,9 +1,9 @@
 class Codeception < Formula
   desc "Full-stack testing PHP framework"
   homepage "https://codeception.com"
-  url "https://github.com/Codeception/Codeception/archive/2.5.0.tar.gz"
-  version "2.5.0"
-  sha256 "ddfd2df45790640063e7c3ddc391306a68fde473f83516a23db95369e04ece30"
+  url "https://codeception.com/codecept.phar"
+  version "2.4.6"
+  sha256 "e41ba47e3576e72b8bd806e11d438a6155198f183cf7efd459e5e575faf8be36"
   depends_on "php"
 
   def install
