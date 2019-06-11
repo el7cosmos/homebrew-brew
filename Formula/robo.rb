db87a3787f683eed4d379b9666071da98761f43b
el7cosmos <el@elabee.me>
2019-06-12 05:14:09 +0700

    robo 1.4.9

--- Formula/robo.rb
+++ Formula/robo.rb
@@ -1,8 +1,8 @@
 class Robo < Formula
   desc "Modern task runner for PHP"
   homepage "https://robo.li"
-  url "https://github.com/consolidation/Robo/releases/download/1.3.4/robo.phar"
-  sha256 "6a6381ebbda3b2552f722c1c80a3b60764bce526a8a3f2dea8028296480eb596"
+  url "https://github.com/consolidation/Robo/releases/download/1.4.9/robo.phar"
+  sha256 "373364b7149645b11f1cfc80752d476ee4b989af5662b6f668b0cb451afb6afd"
   depends_on "php"
 
   def install
