70fa629d1f0b99a66f832bcc1d1db3ce3116ff82
el7cosmos <el@elabee.me>
2019-09-07 15:43:39 +0700

    drupal-console-launcher 1.9.3

--- Formula/drupal-console-launcher.rb
+++ Formula/drupal-console-launcher.rb
@@ -1,8 +1,8 @@
 class DrupalConsoleLauncher < Formula
   desc "DrupalConsole global executable aka Launcher"
   homepage "https://drupalconsole.com/"
-  url "https://github.com/hechoendrupal/drupal-console-launcher/releases/download/1.9.1/drupal.phar"
-  sha256 "fbe3ad39e49a05c4b93121ffd1899945d5e6f216a6d35f303694ed2e66d9a734"
+  url "https://github.com/hechoendrupal/drupal-console-launcher/releases/download/1.9.3/drupal.phar"
+  sha256 "5df498104c8dcd15f287371b2a80504e9d2aaa3b9649a34904b39e7074d97a11"
 
   def install
     bin.install "drupal.phar" => "drupal"
