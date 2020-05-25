21c8c06d9eb30a3336b90cbf64cd044ea206b816
el7cosmos <el@elabee.me>
2020-05-25 09:21:36 +0700

    drupal-check 1.1.2

--- Formula/drupal-check.rb
+++ Formula/drupal-check.rb
@@ -1,9 +1,8 @@
 class DrupalCheck < Formula
   desc "Check Drupal code for deprecations and discover bugs"
   homepage "https://github.com/mglaman/drupal-check"
-  url "https://github.com/mglaman/drupal-check/releases/download/1.0.14/drupal-check.phar"
-  version "1.0.14"
-  sha256 "2152e4aa2cae4d569265e004b859735358c0f45fc2a8927f5221492eda67a4f3"
+  url "https://github.com/mglaman/drupal-check/releases/download/1.1.2/drupal-check.phar"
+  sha256 "07831a859307f490556f2f44a1acdde6073dda8cfa467def0287d1d54025708d"
   depends_on "php"
 
   def install
