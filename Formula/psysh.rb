8167bbb2fab8374c7c9ddbaaee4c18dd635d81db
el7cosmos <el@elabee.me>
2020-05-31 09:12:39 +0700

    psysh 0.10.4

--- Formula/psysh.rb
+++ Formula/psysh.rb
@@ -1,8 +1,8 @@
 class Psysh < Formula
   desc "Runtime developer console, interactive debugger and REPL for PHP"
   homepage "https://psysh.org"
-  url "https://github.com/bobthecow/psysh/releases/download/v0.9.9/psysh-v0.9.9.tar.gz"
-  sha256 "dc466f88dbb00140d95c23f8826e9d235ed407d1360dc35cf90273a7c08acb4e"
+  url "https://github.com/bobthecow/psysh/releases/download/v0.10.4/psysh-v0.10.4.tar.gz"
+  sha256 "c12103b0e36b67776dbcd02966a026fb5c90ef4f26ffbf7df27a89f07381bd00"
   depends_on "php"
 
   def install
