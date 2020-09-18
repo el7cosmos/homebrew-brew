b25af31f02267bd5385bfdb5758f84a3b4d4baf4
el7cosmos <el@elabee.me>
2020-09-19 05:34:56 +0700

    talenta 0.2.2

--- Formula/talenta.rb
+++ Formula/talenta.rb
@@ -1,8 +1,8 @@
 class Talenta < Formula
   desc "Command-line interface for Talenta"
   homepage "https://github.com/el7cosmos/talenta"
-  url "https://github.com/el7cosmos/talenta/releases/download/0.2.0/talenta-0.2.0-x86_64-apple-darwin.tar.gz"
-  sha256 "ad6895ec2aa9472ed63475f2102dc15fb44a020f2f485f5379627b573268334d"
+  url "https://github.com/el7cosmos/talenta/releases/download/0.2.2/talenta-0.2.2-x86_64-apple-darwin.tar.gz"
+  sha256 "a252d04e94a1dc892adc3ceb5eb7cdf791ccd61084fbfa1e25c0b7d42c961c68"
   license any_of: %w[MIT Apache-2.0]
 
   def install
