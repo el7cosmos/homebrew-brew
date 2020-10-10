e4a0aa85346ef69825d31993d04aca4bea5d2ea0
el7cosmos <el@elabee.me>
2020-10-11 00:36:46 +0700

    drupal-check does not build

--- Formula/drupal-check.rb
+++ Formula/drupal-check.rb
@@ -4,6 +4,7 @@ class DrupalCheck < Formula
   url "https://github.com/mglaman/drupal-check/releases/download/1.1.2/drupal-check.phar"
   sha256 "07831a859307f490556f2f44a1acdde6073dda8cfa467def0287d1d54025708d"
   depends_on "php"
+  disable! date: "2020-07-30", because: :does_not_build
 
   def install
     bin.install "drupal-check.phar" => "drupal-check"
