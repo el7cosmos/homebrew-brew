212736c40d84b39d56704730a410ef74f65b8063
el7cosmos <el@elabee.me>
2020-10-11 09:52:39 +0700

    codeception 4.1.7

--- Formula/codeception.rb
+++ Formula/codeception.rb
@@ -1,8 +1,11 @@
 class Codeception < Formula
   desc "Full-stack testing PHP framework"
   homepage "https://codeception.com"
-  url "https://codeception.com/releases/4.1.20200524/codecept.phar"
-  sha256 "78267afc1003bc02807a76f8cf881f869b7f797e1ad6575c7efab883adccc88d"
+  url "https://codeception.com/releases/4.1.20200828/codecept.phar"
+  version "4.1.7"
+  sha256 "3fdcf81cd5b9e15e89862f1a1896543dfe5e48661da032d3cd67c49f11736e62"
+  license "MIT"
+
   depends_on "php"
 
   def install
