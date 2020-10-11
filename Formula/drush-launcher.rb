d122c2673408e7fef576ae529c040c8be6d89983
el7cosmos <el@elabee.me>
2020-10-11 09:10:45 +0700

    drush-launcher 0.7.3

--- Formula/drush-launcher.rb
+++ Formula/drush-launcher.rb
@@ -1,8 +1,8 @@
 class DrushLauncher < Formula
   desc "Small wrapper around Drush for your global $PATH"
   homepage "https://github.com/drush-ops/drush-launcher"
-  url "https://github.com/drush-ops/drush-launcher/releases/download/0.6.0/drush.phar"
-  sha256 "c3f32a800a2f18470b0010cd71c49e49ef5c087f8131eecfe9b686dc1f3f3d4e"
+  url "https://github.com/drush-ops/drush-launcher/releases/download/0.7.3/drush.phar"
+  sha256 "933fba289d593da6ed39f3f5cbf870867aa31df4ee039696a890a3602a29f2b8"
 
   def install
     bin.install "drush.phar" => "drush"
