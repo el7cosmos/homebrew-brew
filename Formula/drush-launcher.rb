717ad9c75ebc25e11d4f5f92c4655ee66c97d8d3
el7cosmos <el@elabee.me>
2020-11-01 13:26:29 +0700

    drush-launcher 0.8.0

--- Formula/drush-launcher.rb
+++ Formula/drush-launcher.rb
@@ -1,8 +1,8 @@
 class DrushLauncher < Formula
   desc "Small wrapper around Drush for your global $PATH"
   homepage "https://github.com/drush-ops/drush-launcher"
-  url "https://github.com/drush-ops/drush-launcher/releases/download/0.7.3/drush.phar"
-  sha256 "933fba289d593da6ed39f3f5cbf870867aa31df4ee039696a890a3602a29f2b8"
+  url "https://github.com/drush-ops/drush-launcher/releases/download/0.8.0/drush.phar"
+  sha256 "97e64b94588eeca5f97ffcb0595c7e3988e685651d898d65c3ae07ca8f9c94c6"
   license "GPL-2.0-or-later"
 
   def install
