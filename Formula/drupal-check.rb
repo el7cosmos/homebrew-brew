97f2dcacc8f8ac49e166bae8534fdc7900ea424f
el7cosmos <el@elabee.me>
2020-12-08 05:31:16 +0700

    disable! should be put before depends_on

--- Formula/drupal-check.rb
+++ Formula/drupal-check.rb
@@ -3,8 +3,9 @@ class DrupalCheck < Formula
   homepage "https://github.com/mglaman/drupal-check"
   url "https://github.com/mglaman/drupal-check/releases/download/1.1.2/drupal-check.phar"
   sha256 "07831a859307f490556f2f44a1acdde6073dda8cfa467def0287d1d54025708d"
-  depends_on "php"
   disable! date: "2020-07-30", because: :does_not_build
+
+  depends_on "php"
 
   def install
     bin.install "drupal-check.phar" => "drupal-check"
