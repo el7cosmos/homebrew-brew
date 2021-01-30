92d9571391248180fb6889309ea9994ea06d4ab7
el7cosmos <el@elabee.me>
2021-01-30 12:43:22 +0700

    codeception 4.1.16

--- Formula/codeception.rb
+++ Formula/codeception.rb
@@ -1,9 +1,9 @@
 class Codeception < Formula
   desc "Full-stack testing PHP framework"
   homepage "https://codeception.com"
-  url "https://codeception.com/releases/4.1.20201116/codecept.phar"
-  version "4.1.12"
-  sha256 "9f0bd9fdffb1e0e3086fa229694f40e4a7586a0dfc15433c4ca7c7db5f06caff"
+  url "https://codeception.com/releases/4.1.20210126/codecept.phar"
+  version "4.1.16"
+  sha256 "ed4519ab0132ba2a32ec1283350868567f308369d86cc0279214028e41395720"
   license "MIT"
 
   livecheck do
