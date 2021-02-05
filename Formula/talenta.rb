433174f87b50cc9ca21f7dd9be9ab389b17195af
el7cosmos <el@elabee.me>
2021-02-05 22:39:37 +0700

    talenta 0.4.2

--- Formula/talenta.rb
+++ Formula/talenta.rb
@@ -1,10 +1,9 @@
 class Talenta < Formula
   desc "Command-line interface for Talenta"
   homepage "https://github.com/el7cosmos/talenta"
-  url "https://github.com/el7cosmos/talenta/archive/0.4.0.tar.gz"
-  sha256 "7a42cacbd7e09a92d977a124752d112ac1a4f5d9c92ec6770dd0f8fb5418bd42"
+  url "https://github.com/el7cosmos/talenta/archive/0.4.2.tar.gz"
+  sha256 "cb982e2c161663bf8c1921d761b368ca1b5d50d16d007ffcce8a118e5269e9c0"
   license any_of: %w[MIT Apache-2.0]
-  revision 1
 
   bottle do
     root_url "https://dl.bintray.com/el7cosmos/bottle-brew"
