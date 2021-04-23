e8c13056aa9a4813d28a0d719f031a3764675329
Abi أب <el@elabee.me>
2021-04-24 05:54:18 +0700

    talenta 0.4.5
    Closes #49.
    
    Signed-off-by: Abi أب <el@elabee.me>

--- Formula/talenta.rb
+++ Formula/talenta.rb
@@ -1,8 +1,8 @@
 class Talenta < Formula
   desc "Command-line interface for Talenta"
   homepage "https://github.com/el7cosmos/talenta"
-  url "https://github.com/el7cosmos/talenta/archive/0.4.4.tar.gz"
-  sha256 "5433870a955d6dbd2bfbbda3b2064f8123e7f111a780a4dbaafcd4d90aa18784"
+  url "https://github.com/el7cosmos/talenta/archive/0.4.5.tar.gz"
+  sha256 "66a1d8a4d9d0013994b88d706d0cef4c8b543f3a89a1077e99de731e9bd0a6b6"
   license any_of: %w[MIT Apache-2.0]
 
   bottle do
