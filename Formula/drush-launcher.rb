691d7ff5b52fb155ce26d5ca6d418c1021a87417
Abi أب <el@elabee.me>
2021-05-20 01:54:23 +0000

    drush-launcher: add 0.9.1 bottle.

--- Formula/drush-launcher.rb
+++ Formula/drush-launcher.rb
@@ -11,6 +11,13 @@ class DrushLauncher < Formula
     url :stable
   end
 
+  bottle do
+    root_url "https://ghcr.io/v2/el7cosmos/brew"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, big_sur:  "fad405732f06c492ef96b37895b6c5ac43dc39a0b607e7078ab4902c2be6bf23"
+    sha256 cellar: :any_skip_relocation, catalina: "0e52b1fa396f83f327896066dafc4074cfcb8a924ccaf49a0f661954807baeb7"
+  end
+
   depends_on "composer" => :build
   depends_on "humbug/box/box" => :build
   depends_on "php"
