e59d83d992766f97611333933afc038ad48b6965
Abi أب <el@elabee.me>
2021-05-24 21:48:37 +0700

    roadrunner 2.2.0
    Closes #54.
    
    Signed-off-by: Abi أب <el@elabee.me>

---
+++ Formula/roadrunner.rb
@@ -0,0 +1,24 @@
+class Roadrunner < Formula
+  desc "High-performance PHP application server, load-balancer & process manager"
+  homepage "https://roadrunner.dev/"
+  url "https://github.com/spiral/roadrunner-binary/archive/refs/tags/v2.2.0.tar.gz"
+  sha256 "3a6aa7fe1a3081d84d0fe98a945586a5e3bbd2b9e862dc3a4225d34900a5c359"
+  license "MIT"
+
+  livecheck do
+    url :stable
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "go",
+           "build",
+           *std_go_args(ldflags: "-s -X github.com/spiral/roadrunner-binary/v2/internal/meta.version=#{version} -X github.com/spiral/roadrunner-binary/v2/internal/meta.buildTime=#{Time.now.iso8601}"),
+           "./cmd/rr"
+  end
+
+  test do
+    system "#{bin}/roadrunner", "-v"
+  end
+end
