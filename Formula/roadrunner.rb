08c6ee72e5f459b90fe0f24e30fde063ff9bba5a
Abi أب <el@elabee.me>
2021-05-24 15:00:20 +0000

    roadrunner: add 2.2.0 bottle.

--- Formula/roadrunner.rb
+++ Formula/roadrunner.rb
@@ -9,6 +9,12 @@ class Roadrunner < Formula
     url :stable
   end
 
+  bottle do
+    root_url "https://ghcr.io/v2/el7cosmos/brew"
+    sha256 cellar: :any_skip_relocation, big_sur:  "d9ac3b5aa1c974d4eb8a15f42d1e50b9f5bf0125236397aa329ce158d849fefb"
+    sha256 cellar: :any_skip_relocation, catalina: "03d4579cfd5fb7450834916c75cc4ea8859293bb7c11f2ff13817f2e951c0fd6"
+  end
+
   depends_on "go" => :build
 
   def install
