b22f289f4d8bfb019476b9643e5016b0c06c4d0b
Abi أب <el@elabee.me>
2021-07-17 09:59:04 +0700

    roadrunner 2.3.3

--- Formula/roadrunner.rb
+++ Formula/roadrunner.rb
@@ -1,8 +1,8 @@
 class Roadrunner < Formula
   desc "High-performance PHP application server, load-balancer & process manager"
   homepage "https://roadrunner.dev/"
-  url "https://github.com/spiral/roadrunner-binary/archive/refs/tags/v2.3.0.tar.gz"
-  sha256 "381d83b48c0403fd3dccf26002d6d87a64fe7142819f1bb953bdf1dd60c230de"
+  url "https://github.com/spiral/roadrunner-binary/archive/refs/tags/v2.3.3.tar.gz"
+  sha256 "8fb3fa9735a462837762e57f650b55e0cd6380d9269bafdb1916b76b6f9ee395"
   license "MIT"
 
   livecheck do
